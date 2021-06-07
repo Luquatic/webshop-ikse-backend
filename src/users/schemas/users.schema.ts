8fddc39cdc272254b8f4b3eb4f3460a808dd01fe
Jessey <jesseyfransen@live.nl>
2021-06-07 14:49:09 +0200

    add userdto

---
+++ src/users/schemas/users.schema.ts
@@ -0,0 +1,24 @@
+import {Prop, Schema, SchemaFactory} from '@nestjs/mongoose';
+import {Document} from 'mongoose';
+
+export type UserDocument = User & Document;
+
+@Schema()
+export class User {
+    @Prop()
+    firstname: string;
+
+    @Prop()
+    lastname: string;
+
+    @Prop()
+    username: string;
+
+    @Prop()
+    password: string;
+
+    @Prop()
+    isAdmin: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
